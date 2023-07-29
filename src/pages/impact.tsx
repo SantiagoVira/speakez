8ccf7521e38861b4f24c4c7e57ff6a2a9882f457
SantiagoVira <santiagovira06@gmail.com>
2023-07-29 13:17:35 -0400

    add layouts to other pages

--- src/pages/impact.tsx
+++ src/pages/impact.tsx
@@ -1,5 +1,7 @@
+import Layout from "~/components/shared/layout";
+
 const Impact: React.FC = () => {
-  return <></>;
+  return <Layout></Layout>;
 };
 
 export default Impact;
--- src/pages/info.tsx
+++ src/pages/info.tsx
@@ -1,5 +1,7 @@
+import Layout from "~/components/shared/layout";
+
 const Info: React.FC = () => {
-  return <></>;
+  return <Layout></Layout>;
 };
 
 export default Info;
